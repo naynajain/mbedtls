daxtens/mbedtls-1

mbedtls: generate_features.pl updates features file for pkcs7
This patch adds the updates generated by running generate_features.pl
for pkcs7.

Signed-off-by: Nayna Jain <nayna@linux.ibm.com>

programs/test/query_config.c
@@ -2162,6 +2162,14 @@ int query_config( const char *config )
     }
 #endif /* MBEDTLS_VERSION_C */
 
+#if defined(MBEDTLS_PKCS7_USE_C)
+    if( strcmp( "MBEDTLS_PKCS7_USE_C", config ) == 0 )
+    {
+        MACRO_EXPANSION_TO_STR( MBEDTLS_PKCS7_USE_C );
+        return( 0 );
+    }
+#endif /* MBEDTLS_PKCS7_USE_C */
+
 #if defined(MBEDTLS_X509_USE_C)
     if( strcmp( "MBEDTLS_X509_USE_C", config ) == 0 )
     {

naynajain/mbedtls

programs/ssl/query_config.c
@@ -2051,6 +2051,14 @@ int query_config( const char *config )
     }
 #endif /* MBEDTLS_VERSION_C */
 
+#if defined(MBEDTLS_PKCS7_C)
+    if( strcmp( "MBEDTLS_PKCS7_C", config ) == 0 )
+    {
+        MACRO_EXPANSION_TO_STR( MBEDTLS_PKCS7_C );
+        return( 0 );
+    }
+#endif /* MBEDTLS_PKCS7_C */
+
 #if defined(MBEDTLS_X509_USE_C)
     if( strcmp( "MBEDTLS_X509_USE_C", config ) == 0 )
     {